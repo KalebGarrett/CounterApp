7f01029e4847c963a9690b44542fb317f1cc115d
Kaleb Garrett <kalebagarrett@gmail.com>
2022-12-07 14:56:47 -0500

    Added functionality.

--- CounterApp.Web/wwwroot/js/site.js
+++ CounterApp.Web/wwwroot/js/site.js
@@ -1,1 +1,33 @@
-﻿
+﻿let addition = document.querySelector("#add");
+let subtraction = document.querySelector("#subtract");
+let reset = document.querySelector("#reset");
+
+addition.addEventListener("click", function () {
+    let output = document.querySelector("#output")
+    let result = Number(output.innerHTML) + 1;
+    
+    if (result > 9) {
+        result = 0;
+    }
+
+    output.innerText = result;
+});
+
+subtraction.addEventListener("click", function () {
+    let output = document.querySelector("#output")
+    let result = Number(output.innerHTML) - 1;
+
+    if (result < 0) {
+        result = 0;
+    }
+    
+    output.innerText = result;
+});
+
+reset.addEventListener("click", function () {
+    let output = document.querySelector("#output");
+    let result;
+    result = 0;
+    
+    output.innerHTML = result;
+});
